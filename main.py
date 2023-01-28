64478a9075d59fb508b7d60087cfbd4591978cb0
pcdogyu <pcdogyu@hotmail.com>
2023-01-28 14:55:52 +0800

    add refresh in 5 second

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 import pandas as pd
 from datetime import datetime
 pd.set_option('expand_frame_repr',False)
@@ -18,12 +19,26 @@ def get_tickers_data(symbol_list):
         tickers_df = pd.concat([tickers_df,ticker_df],axis=0)
     return tickers_df
 
+def sleeptime(hour,min,sec):
+    return hour*3600 + min*60 + sec
+
 def main():
     symbol_list = ['BTCUSDT','ETHUSDT','BCHUSDT','LTCUSDT','XRPUSDT','XLMUSDT']
     tickers_df = get_tickers_data(symbol_list)
+    
     print(tickers_df[['symbol','askQty','askPrice','bidPrice', 'bidQty','volume', 'closeTime']])
 
     
-    
-if __name__ == '__main__':
+# if __name__ == '__main__':
+#     second = sleeptime (0,0,5)    
+#     time.sleep(second)   
+#     main()
+
+seconds = sleeptime (0,0,5)
+while 1 == 1:
+    time.sleep(seconds)
     main()
+    
+    print(datetime.now())
+    print("wait for 5 second")
+    
